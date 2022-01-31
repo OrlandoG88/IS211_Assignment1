3232f845ffe59a1369cecd9b0e0a2c346273be1e
OrlandoG88 <98724886+OrlandoG88@users.noreply.github.com>
2022-01-31 15:14:28 -0500

    first assignment

---
+++ assingmnet1_part1.py
@@ -0,0 +1,23 @@
+def listDivide(numbers, divide = 2):
+    Count = 0
+    for x in numbers:
+        if x % divide == 0:
+        Count += 1
+        return Count
+
+class ListDivideException(Exception):
+    pass
+
+def testListDivide():
+try:
+    if not listDivide([1,2,3,4,5]) == 2:
+        raise ListDivideException
+    if not listDivide([2,4,6,8,10]) == 5:
+        raise ListDivideException
+    if not listDivide([30,54,63,98,100], divide = 10) == 2:
+        raise ListDivideException
+    if not listDivide([]) == 0:
+        raise ListDivideException
+finally: print("no errors")
+
+testListDivide()
