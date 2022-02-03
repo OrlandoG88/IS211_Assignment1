d523ad3e4dc1e8b16f35f6cbe12f67364b3188b4
OrlandoG88 <98724886+OrlandoG88@users.noreply.github.com>
2022-02-02 19:54:54 -0500

    first assignment

--- assingmnet1_part1.py
+++ assingmnet1_part1.py
@@ -15,13 +15,13 @@ def testListDivide():
     try:
         if not listDivide([1, 2, 3, 4, 5]) == 2:
             raise ListDivideException
-        if not listDivide([2, 4, 6, 8, 10]) == 5:
+        elif not listDivide([2, 4, 6, 8, 10]) == 5:
             raise ListDivideException
-        if not listDivide([30, 54, 63, 98, 100], divide=10) == 2:
+        elif not listDivide([30, 54, 63, 98, 100], divide=10) == 2:
             raise ListDivideException
-        if not listDivide([]) == 0:
+        elif not listDivide([]) == 0:
             raise ListDivideException
-        if not listDivide([1, 2, 3, 4, 5], 1) == 5:
+        elif not listDivide([1, 2, 3, 4, 5], 1) == 5:
             raise ListDivideException
         print("No errors")
     except ListDivideException:
